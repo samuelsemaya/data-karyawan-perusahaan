f6257beb47ca5fa63eb775975f6703e19b28e147
Samuel Semaya <98575520+samuelsemaya@users.noreply.github.com>
2024-05-22 00:28:20 +0700

    Add files via upload

--- data karyawan perusahaan/functions/database.py
+++ data karyawan perusahaan/functions/database.py
@@ -11,8 +11,9 @@ data = [
         {'No.':'','NIK':pk(6),'Nama':'John','Umur':25,'Jenis Kelamin':'Pria','Alamat':'Jakarta','Divisi':'IT','Level':'Manager','Jabatan':'Developer','Gaji':200000},
         {'No.':'','NIK':pk(6),'Nama':'Diana','Umur':30,'Jenis Kelamin':'Wanita','Alamat':'Surabaya','Divisi':'HRD','Level':'Staf','Jabatan':'HR Manager','Gaji':100000},
         {'No.':'','NIK':pk(6),'Nama':'Bob','Umur':22,'Jenis Kelamin':'Pria','Alamat':'Bandung','Divisi':'IT','Level':'Manager','Jabatan':'Developer','Gaji':150000},
-        {'No.':'','NIK':pk(6),'Nama':'Budi','Umur':22,'Jenis Kelamin':'Pria','Alamat':'Bandung','Divisi':'IT','Level':'Staf','Jabatan':'Developer','Gaji':150000},
-        {'No.':'','NIK':pk(6),'Nama':'Lala','Umur':22,'Jenis Kelamin':'Wanita','Alamat':'Jakarta','Divisi':'Marketing','Level':'Staf','Jabatan':'Digital Marketing','Gaji':100000}
+        {'No.':'','NIK':pk(6),'Nama':'Budi Santoso','Umur':22,'Jenis Kelamin':'Pria','Alamat':'Bandung','Divisi':'IT','Level':'Staf','Jabatan':'Developer','Gaji':150000},
+        {'No.':'','NIK':pk(6),'Nama':'Lala','Umur':22,'Jenis Kelamin':'Wanita','Alamat':'Jakarta','Divisi':'Marketing','Level':'Staf','Jabatan':'Digital Marketing','Gaji':100000},
+        {'No.':'','NIK':pk(6),'Nama':'Budi Rahmat','Umur':30,'Jenis Kelamin':'Pria','Alamat':'Jakarta','Divisi':'Marketing','Level':'Manager','Jabatan':'Digital Marketing','Gaji':250000}
         ]
         
 for index, value in enumerate(data, 1):
